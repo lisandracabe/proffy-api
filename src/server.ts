b50a2e9700207a8d59d6c6fefa155cb6c4800d54
= <=>
2020-08-04 17:21:05 -0300

    feature: implementação da rota post /classes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) =>{
-    res.json({message: 'hello world'});
-});
+app.use(routes);
 
 app.listen(3333);
